fefadcbda84f900b87c96caac16217cc298baf05
soliforte <soliforte@protonmail.com>
2017-08-17 16:09:54 -0400

    Added Clustering for Network markers. Filtering on Networks only, not clients

--- plugin-kismap/httpd/js/kismap.js
+++ plugin-kismap/httpd/js/kismap.js
@@ -17,11 +17,14 @@ kismet_ui_tabpane.AddTab({
 	expandable: true,
 	createCallback: function(div) {
     $(document).ready( function() {
-      $.getScript('/plugin/kismap/js/leaflet.js')
+      //$.getScript('/plugin/leaflet/js/leaflet.js')
       $(div).append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
       $(div).append('<link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />');
-      $(div).append('<link rel="stylesheet" href="/plugin/kismap/leaflet.css">');
-      $(div).append('<script src="/plugin/kismap/js/leaflet.js"></script>');
+      $(div).append('<link rel="stylesheet" href="/plugin/leaflet/leaflet.css">');
+      $(div).append('<script src="/plugin/leaflet/js/leaflet.js"></script>');
+      $(div).append('<link rel="stylesheet" href="/plugin/leaflet/MarkerCluster.css">')
+      $(div).append('<script src="/plugin/leaflet/js/leaflet.markercluster.js">')
+
       var mymap = L.map('mapid').setView([40.775,-73.972], 15);
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                     maxZoom: 18,
@@ -39,25 +42,30 @@ kismet_ui_tabpane.AddTab({
         function onLocationFound(e) {
             console.log(e);
         // e.heading will contain the user's heading (in degrees) if it's available, and if not it will be NaN. This would allow you to point a marker in the same direction the user is pointed.
-            L.marker(e.latlng).addTo(mymap);
-            L.popup().setLatLng(e.latlng).setContent("Operator").openOn(mymap);
+            var marker = L.marker(e.latlng).addTo(mymap);
+            marker.bindPopup("Operator").openPopup();
       }
 
-      $( window ).ready( function() {
-        $.getJSON("localhost:2501/devices/by-ts/-60/devices.json").done(function(data) {
-        var mac = data['kismet.device.base.macaddr'];
-        var lat = data['kismet.common.signal.peak_loc']['kismet.common.location.lat'];
-        var lon = data['kismet.common.signal.peak_loc']['kismet.common.location.lon'];
-        var coordinates = lat + lon;
-        for (var x = 0; x < data.length; x++) {
-           var marker = L.marker([lat, lon]).addTo(mymap);
-           var popop = L.popup()
-              .setLatLng([lat, lon])
-              .setContent(mac)
-              .openOn(mymap);
-         }
+      $( mymap ).ready( function() {
+        var dataCluster = new L.MarkerClusterGroup();
+        $.getJSON("/devices/last-time/-5/devices.json").done(function(data) {
+            console.log(mac, lat, lon);
+            for (var x = 0; x < data.length; x++) {
+              var ssid = data[x]['kismet.device.base.name'];
+              var type = data[x]['kismet.device.base.type'];
+              var mac = data[x]['kismet.device.base.macaddr'];
+              var lat = data[x]['kismet.device.base.signal']['kismet.common.signal.peak_loc']['kismet.common.location.lat'];
+              var lon = data[x]['kismet.device.base.signal']['kismet.common.signal.peak_loc']['kismet.common.location.lon'];
+              if (type = 'Wi-Fi AP'){
+                console.log(ssid, type, mac, lat, lon);
+                var popup = "<b>" + ssid + "</b><br>" + mac;
+                var marker = L.marker([lat, lon]).addTo(mymap).bindPopup( popup );
+                dataCluster.addLayer( marker );
+              }
+            }
+            mymap.addLayer( dataCluster );
+          })
       })
-    })
     })
 	},
 	priority:    -999,
