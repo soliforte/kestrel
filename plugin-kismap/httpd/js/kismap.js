b06ff0ca2f6d70dc69d892760f8f1299a8808364
soliforte <soliforte@protonmail.com>
2017-08-18 21:19:17 -0400

    Built most recent leaflet.markecluster. Linted code

--- plugin-kismap/httpd/js/kismap.js
+++ plugin-kismap/httpd/js/kismap.js
@@ -16,14 +16,16 @@ kismet_ui_tabpane.AddTab({
 	tabTitle:    'Maps',
 	createCallback: function(div) {
     $(document).ready( function() {
-      //$.getScript('/plugin/leaflet/js/leaflet.js')
+
       $(div).append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
       $(div).append('<link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />');
       $(div).append('<link rel="stylesheet" href="/plugin/kismap/leaflet.css">');
       $(div).append('<script src="/plugin/kismap/js/leaflet.js"></script>');
       $(div).append('<link rel="stylesheet" href="/plugin/kismap/MarkerCluster.css">')
       $(div).append('<script src="/plugin/kismap/js/leaflet.markercluster.js">')
-
+      //Instantiate cluster for le clustering of devices
+      var dataCluster = new L.MarkerClusterGroup();
+      //Instantiate le map
       var mymap = L.map('mapid').setView([40.775,-73.972], 15);
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                     maxZoom: 18,
@@ -32,11 +34,12 @@ kismet_ui_tabpane.AddTab({
                             'Imagery © <a href="http://mapbox.com">Mapbox</a>',
                     id: 'mapbox.streets'
             }).addTo(mymap);
-
+      //Probably removing this. Gets current location via browser API
       $( window ).ready( function(){
-          mymap.locate({setView: true, maxZoom: 15});
+          mymap.locate({setView: true, maxZoom: 15, watch: true});
       });
 
+      //Once location is found, drop a marker on that location
       mymap.on('locationfound', onLocationFound);
         function onLocationFound(e) {
             console.log(e);
@@ -45,9 +48,10 @@ kismet_ui_tabpane.AddTab({
             marker.bindPopup("Operator").openPopup();
       }
 
-      $( mymap ).ready( function() {
-        var dataCluster = new L.MarkerClusterGroup();
-        $.getJSON("/devices/last-time/-5/devices.json").done(function(data) {
+      //Main routine, this gets devices and plots them
+      $(mymap).ready(function() {
+        //Get devices within the last n seconds. Make this throttle-able with a form??
+        $.getJSON("/devices/last-time/-60/devices.json").done(function(data) {
             console.log(mac, lat, lon);
             for (var x = 0; x < data.length; x++) {
               var ssid = data[x]['kismet.device.base.name'];
@@ -58,17 +62,17 @@ kismet_ui_tabpane.AddTab({
               if (type = 'Wi-Fi AP'){
                 console.log(ssid, type, mac, lat, lon);
                 var popup = "<b>" + ssid + "</b><br>" + mac;
-                var marker = L.marker([lat, lon]).addTo(mymap).bindPopup( popup );
+                var marker = new L.marker([lat, lon]).addTo(mymap).bindPopup( popup );
                 dataCluster.addLayer( marker );
               }
             }
             mymap.addLayer( dataCluster );
-          })
-      })
-    })
-	},
-	priority:    -999,
-});
+          }); //end of getJSON
+        }); //end of mymap.ready
+      }); //end of document.ready
+    }, //end of function(div)
+	   priority:    -999,
+   }); //End of createCallback
 // We're done loading
 exports.load_complete = 1;
 return exports;
